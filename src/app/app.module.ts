bf86f6582d5f5ca8327c2950288f595c01ec9e89
ross <ross.fleming@serviceware.se>
2019-06-12 15:53:21 +0200

    restart. angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products.component';
-import { ProductComponent } from './product/product.component';
-import { ProductsService } from './products.service';
-import { HomeComponent } from './home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, ProductComponent, HomeComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ProductsService],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
