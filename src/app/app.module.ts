4e174c870d6e8cfb8dde01a5c47cc878ca956a61
ross <ross.fleming@serviceware.se>
2019-06-12 16:36:19 +0200

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRoutingmodule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent, LoginComponent, TrainingComponent, CurrentTrainingComponent, NewTrainingComponent, PastTrainingComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    WelcomeComponent
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AppRoutingmodule],
   providers: [],
   bootstrap: [AppComponent]
 })
