c794edfdfefa215890536ba1b5bb3d7d54804a52
ross <ross.fleming@serviceware.se>
2019-06-12 14:58:11 +0200

    product and products components added. routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsService } from './products.service';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, ProductsComponent, ProductComponent, HomeComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
