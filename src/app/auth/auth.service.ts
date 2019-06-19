3a6e0ff4d47959621129a64e712160ae1e0df176
ross <ross.fleming@serviceware.se>
2019-06-19 13:36:57 +0200

    refactor

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,26 +3,45 @@ import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs/Subject';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { TrainingService } from '../training/training.service';
 
 @Injectable()
-// This service allows us to fake a user login, inform other parts of the app about the login
 export class AuthService {
     authChange = new Subject<boolean>();
     private isAuthenticated = false;
-    private user: User;
 
     constructor(
         private router: Router,
-        private afauth: AngularFireAuth,
-        private trainingService: TrainingService,
-        snackbar: MatSnackBar
+        private afAuth: AngularFireAuth,
+        private trainingService: TrainingService
     ) { }
 
+    initAuthListener() {
+        this.afAuth.authState.subscribe(user => {
+            if (user) {
+                // if we are swithcing to authenticated...
+                // set authenticated = true
+                this.isAuthenticated = true;
+                // we will emit an event
+                this.authChange.next(true);
+                // we will redirect to training
+                this.router.navigate(['/training']);
+            } else {
+                // else we will cancel subscriptions
+                this.trainingService.cancelSubscriptions();
+                // we will emit an event
+                this.authChange.next(false);
+                // we will redirect to the login screen
+                this.router.navigate(['/login']);
+                // set authenticated = false
+                this.isAuthenticated = false;
+            }
+        })
+    }
+
     registerUser(authData: AuthData) {
-        this.afauth.auth
+        this.afAuth.auth
             .createUserWithEmailAndPassword(
                 authData.email,
                 authData.password
@@ -33,11 +52,10 @@ export class AuthService {
             .catch(error => {
                 console.log(error)
             })
-        this.isAuthSuccessfully();
     }
 
     login(authData: AuthData) {
-        this.afauth.auth
+        this.afAuth.auth
             .signInWithEmailAndPassword(
                 authData.email,
                 authData.password
@@ -47,28 +65,20 @@ export class AuthService {
             .catch(error => {
                 console.log(error)
             })
-        this.isAuthSuccessfully();
         // this.user = {
         //     email: authData.email,
         //     // fake random userId until the server is implemented
         //     userId: Math.round(Math.random() * 10000).toString()
         // };
     }
+
     logout() {
-        this.trainingService.cancelSubscriptions()
-        this.afauth.auth.signOut()
-        this.authChange.next(false);
-        this.router.navigate(['/login'])
-        this.isAuthenticated = false
+        this.afAuth.auth.signOut();
     }
 
     isAuth() {
         return this.isAuthenticated
     }
 
-    private isAuthSuccessfully() {
-        this.isAuthenticated = true;
-        this.authChange.next(true);
-        this.router.navigate(['/training'])
-    }
+
 }
